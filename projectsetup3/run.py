ad4266e2780867a96c3e47646c4ceb2a83bb1f32
QuittoGames <brunamoreiraquitto@gmail.com>
2025-12-27 14:20:00 -0300

    1.0v

---
+++ projectsetup3/run.py
@@ -0,0 +1,8 @@
+"""
+Ponto de entrada para o executável PyInstaller.
+Este arquivo evita problemas com importações relativas.
+"""
+from projectsetup3.cli import main
+
+if __name__ == "__main__":
+    main()
