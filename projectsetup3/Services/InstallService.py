3ab342fcd4ecbf6341abd866b2fcc33047f74428
Quitto <brunamoreiraquitto@gmail.com>
2025-12-31 15:03:49 -0300

    Add Install Service

--- projectsetup3/Services/InstallService.py
+++ projectsetup3/Services/InstallService.py
@@ -14,15 +14,16 @@ class InstallService:
     def install(config: Config) -> None:
         appdata_local = Path(os.getcwd()) / "appdata" / "Languages"
 
-        if not appdata_local.exists():
-            return
+        if not appdata_local.exists():return
 
-        if Path(config.appdata).exists():
-            return
+        if Path(config.appdata / "PROJECTSETUP-3.O").exists():return
 
+        Path(config.appdata / "PROJECTSETUP-3.O").parent.mkdir(parents=True, exist_ok=True)
+        Path(config.appdata / "History").parent.mkdir(parents=True,exist_ok=True)
         shutil.move(str(appdata_local), str(config.appdata))
+    
         InstallService.setEnv()
-        
+
     @staticmethod
     def setEnv() -> None:
         command = (
